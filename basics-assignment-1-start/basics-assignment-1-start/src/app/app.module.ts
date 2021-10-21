ee970b508e14b6a65fd6f1eb1c8192d069e4e490
Lindsay Gillespie <lind-gillespie@sbcglobal.net>
2021-10-21 12:38:46 -0500

    Added first component

--- basics-assignment-1-start/basics-assignment-1-start/src/app/app.module.ts
+++ basics-assignment-1-start/basics-assignment-1-start/src/app/app.module.ts
@@ -3,10 +3,12 @@ import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
+import { warningAlertComponent } from './warningAlert/warningAlert.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    warningAlertComponent
   ],
   imports: [
     BrowserModule,
---
+++ basics-assignment-1-start/basics-assignment-1-start/src/app/warningAlert/warningAlert.component.ts
@@ -0,0 +1,14 @@
+import { Component } from "@angular/core";
+
+@Component({
+    selector:'app-warningAlert',
+    templateUrl:'./warningAlert.component.html',
+    styles:[`
+    h1{
+        color:yellow
+    }
+    `]
+})
+export class warningAlertComponent{
+
+}
