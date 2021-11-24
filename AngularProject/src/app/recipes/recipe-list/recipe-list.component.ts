357d56a55cc9eb0c343888fb173925de10fe2ea3
Lindsay Gillespie <lind-gillespie@sbcglobal.net>
2021-11-24 09:41:48 -0600

    Added in a Picture and change the link

--- AngularProject/src/app/recipes/recipe-list/recipe-list.component.ts
+++ AngularProject/src/app/recipes/recipe-list/recipe-list.component.ts
@@ -8,7 +8,7 @@ import { Recipe } from '../recipe.model';
 })
 export class RecipeListComponent implements OnInit {
   recipes:Recipe[]=[
-    new Recipe('A test Recipe','This is a test','https://www.google.com/search?q=recipe+card&tbm=isch&ved=2ahUKEwi3j867taD0AhVO_6wKHTAbBvMQ2-cCegQIABAA&oq=recipe+card&gs_lcp=CgNpbWcQAzIICAAQgAQQsQMyBQgAEIAEMgUIABCABDIFCAAQgAQyBQgAEIAEMgUIABCABDIFCAAQgAQyBQgAEIAEMgUIABCABDIFCAAQgAQ6BwgjEO8DECc6BwgAELEDEEM6BAgAEENQigVY3gtgzg5oAHAAeACAAWiIAboEkgEDNS4xmAEAoAEBqgELZ3dzLXdpei1pbWfAAQE&sclient=img&ei=XX6VYffsM87-swWwtpiYDw&bih=754&biw=1536&rlz=1C1VDKB_enUS931US931#imgrc=jN2DPTDSH4AhsM')
+    new Recipe('A test Recipe','This is a test','https://github.com/LinGill21/AngularUdemy/tree/Project/AngularProject/recipeCard.jpg')
   ];
 
   constructor() { }
